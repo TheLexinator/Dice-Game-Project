f48efdb39fc1ed68e87ce3651d0bb2d575f268ca
Akhona <akhonabest7@gmail.com>
2023-10-25 06:18:48 +0200

    let the Lexinator do her magic

--- script.js
+++ script.js
@@ -7,17 +7,28 @@ let player2Score = 0;
 let currentPlayer = 1;
 
 // Ask for player names
-const player1Name = prompt("Enter Player 1's Name:");
-const player2Name = prompt("Enter Player 2's Name:");
-console.log ("Let the games begin")
-document.getElementById('player1').querySelector('h2').textContent = player1Name;
-document.getElementById('player2').querySelector('h2').textContent = player2Name;
+const editNamesBtn = document.getElementById('editNamesBtn');
+const player1NameElement = document.getElementById('player1').querySelector('h2');
+const player2NameElement = document.getElementById('player2').querySelector('h2');
+
+editNamesBtn.addEventListener('click', () => {
+    const newPlayer1Name = prompt("Enter Player 1's Name:");
+    const newPlayer2Name = prompt("Enter Player 2's Name:");
+    
+    if (newPlayer1Name) {
+        player1NameElement.textContent = newPlayer1Name;
+    }
+    
+    if (newPlayer2Name) {
+        player2NameElement.textContent = newPlayer2Name;
+    }
+});
 
 function determineWinner() {
     if (player1Score > player2Score) {
-        alert(`${player1Name} wins! `);
+        alert(`${player1NameElement.textContent} wins!`);
     } else if (player2Score > player1Score) {
-        alert(`${player2Name} wins! `);
+        alert(`${player2NameElement.textContent} wins!`);
     } else {
         alert("It's a tie!");
     }
@@ -28,12 +39,13 @@ rollBtn.addEventListener('click', () => {
     if (currentPlayer === 1) {
         player1Score += diceValue;
         score1.textContent = player1Score;
-        currentPlayer = 2;
     } else {
         player2Score += diceValue;
         score2.textContent = player2Score;
-        determineWinner();
     }
+
+    determineWinner();
+    currentPlayer = currentPlayer === 1 ? 2 : 1; // Toggle players
 });
 
 newGameBtn.addEventListener('click', () => {
@@ -43,36 +55,21 @@ newGameBtn.addEventListener('click', () => {
     score2.textContent = player2Score;
     currentPlayer = 1;
 });
-// Existing code for HTML elements and player scores
 
+// Winner determination logic (you can keep one of these)
 const winnerSection = document.getElementById('winnerSection');
 const winnerText = document.getElementById('winner');
 
 function determineWinner() {
     if (player1Score > player2Score) {
-        winnerText.textContent = `${player1Name} wins !!!🏆`;
+        winnerText.textContent = `${player1NameElement.textContent} wins! 🏆`;
     } else if (player2Score > player1Score) {
-        winnerText.textContent = `${player2Name} wins !!! 🏆`;
+        winnerText.textContent = `${player2NameElement.textContent} wins! 🏆`;
     } else {
         winnerText.textContent = "It's a tie!";
     }
 
-    // Display the winner section
     winnerSection.style.display = 'block';
 }
+// Initialize variables to track rolls for each player
 
-// Attach event listener to the roll button (existing code)
-rollBtn.addEventListener('click', () => {
-    const diceValue = Math.floor(Math.random() * 6) + 1;
-    if (currentPlayer === 1) {
-        player1Score += diceValue;
-        score1.textContent = player1Score;
-        currentPlayer = 2;
-    } else {
-        player2Score += diceValue;
-        score2.textContent = player2Score;
-        determineWinner();
-    }
-});
-
-// Attach event listener to the new game button (existing code)
